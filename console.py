82a38e7e933c1f65b9016c68920c274aca1104be
abey546 <131441171+abey546@users.noreply.github.com>
2024-02-09 19:32:20 +0300

    updated

--- console.py
+++ console.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3 
 import cmd
+from models.__init__ import storage
+from models.base_model import BaseModel
 class HBNBCommand(cmd.Cmd):
     """Contains the functionality for the HBNB console"""
     prompt = "(hbnb) "
@@ -9,5 +12,19 @@ class HBNBCommand(cmd.Cmd):
         """EOF signal to exit the program."""
         print()
         return True
+    def do_create(self, args):
+        tokens = args.split(" ")
+        """Create an object of any class"""
+        if not args:
+            print("** class name missing **")
+            return
+        elif tokens[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[tokens[0]]()
+        storage.new(new_instance)
+        storage.save()
+        print(new_instance.id)
+
 if __name__ == '__main__' :
     HBNBCommand().cmdloop()
